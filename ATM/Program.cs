52d390585b0f07ad59dc819803d61af1bdc0acba
noelw19 <noelw19@outlook.com>
2021-06-20 13:37:44 +1200

    initial commit

---
+++ ATM/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ViewLayer;
+using B0Layer;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            View view = new View();
+            view.loginScreen();
+
+            
+            
+            List<string> strList = new List<string>();
+            strList.Add("Test");
+            strList.Add("Of");
+            strList.Add("NetCore");
+
+            foreach(var str in strList){
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
